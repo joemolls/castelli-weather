6db9810275f217dc07b0f0619689ba84a04f199d
Gianfranco Mollo <gmollo@redhat.com>
2026-02-18 01:07:30 +0100

    aggiunta pagina dei percorsi

--- locations.py
+++ locations.py
@@ -1,26 +1,38 @@
 LOCATIONS = {
     "monte-cavo": {
         "name": "Monte Cavo",
-        "lat": 41.747,
-        "lon": 12.700,
+        "lat": 41.7517,    
+        "lon": 12.710,    
         "elevation": 949
+    },
+    "colle-jano": {
+        "name": "Colle Jano",
+        "lat": 41.757,     
+        "lon": 12.726,   
+        "elevation": 938
     },
     "maschio-faete": {
         "name": "Maschio delle Faete",
-        "lat": 41.765,
-        "lon": 12.715,
+        "lat": 41.7569,    
+        "lon": 12.7442,   
         "elevation": 956
+    },
+    "maschio-ariano": {
+        "name": "Maschio d'Ariano",
+        "lat": 41.7394,  
+        "lon": 12.7908,
+        "elevation": 891
     },
     "maschio-artemisio": {
         "name": "Maschio d'Artemisio",
-        "lat": 41.717,
-        "lon": 12.750,
-        "elevation": 891
+        "lat": 41.7122,   
+        "lon": 12.7534,    
+        "elevation": 812
     },
     "fontana-tempesta": {
         "name": "Fontana Tempesta",
-        "lat": 41.760,
-        "lon": 12.730,
-        "elevation": 606
+        "lat": 41.735,     
+        "lon": 12.712,    
+        "elevation": 560
     }
 }
